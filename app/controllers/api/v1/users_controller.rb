1b2c89f707a7fa371c0bf6e6b16ec39091d6f507
minguu <minguu42@gmail.com>
2020-11-16 09:29:34 +0900

    userを包み、uuidをサーバー側で作成

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,15 +5,15 @@ module Api
       def create
         user = User.new(user_params)
         if user.save
-          render json: user
+          render json: {user: user}
         else
-          render json: user.errors
+          render json: {user: user.errors}
         end
       end
 
       private
         def user_params
-          params.require(:user).permit(:uuid, :name, :last_used_at)
+          params.require(:user).permit(:name, :last_used_at)
         end
     end
   end
