14973f19ae887fe4e97f681c97006b79a4a3fd0d
minguu <minguu42@gmail.com>
2020-11-16 10:08:02 +0900

    jugyo作成のAPIの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
+      resources :jugyos, only: [:create]
       resources :universities, only: [:index]
     end
   end
