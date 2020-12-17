4e7a5066f2e229a6d9792e1f5377f0be7ee72c97
H0R15H0 <shohori13@gmail.com>
2020-12-17 23:46:44 +0900

    message_broadcast_jobでuser_uuidも返すようにした。

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,5 +1,5 @@
 class MessageBroadcastJob < ApplicationJob
   def perform(user_room_message)
-    ActionCable.server.broadcast("room_channel_#{user_room_message.room.uuid}", message: user_room_message.message, send_at: user_room_message.send_at)
+    ActionCable.server.broadcast("room_channel_#{user_room_message.room.uuid}", message: user_room_message.message, user_uuid: user_room_message.user.uuid, send_at: user_room_message.send_at)
   end
 end
