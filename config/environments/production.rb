3d884f734fc81b2e0cd53b14d85383c5b222b3d9
H0R15H0 <shohori13@gmail.com>
2020-12-17 23:49:42 +0900

    cors, actioncable周りのproduction用の設定を追加。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.disable_request_forgery_protection = true
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 			)
 		end
 
-		# if Rails.env.production?
-    # end
+    if Rails.env.production?
+      origins "*"
+			resource(
+				"*",
+				headers: :any,
+				methods: [:get, :post, :put, :patch, :delete, :options, :head],
+			)
+    end
 	end
 end
